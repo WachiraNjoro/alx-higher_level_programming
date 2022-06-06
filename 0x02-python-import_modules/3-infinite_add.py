2c06e660853384901742043942cfbfe1315e6c5a
WachiraNjoro <dennisnwachira@gmail.com>
2022-06-06 14:55:27 -0700

    Updated

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-output = 0
-for n in range(1, len(sys.argv)):
-    output += int(sys.argv[n])
-    print("{}".format(output))
+    result = 0
+    for i in range(1, len(sys.argv)):
+        result += int(sys.argv[i])
+    print("{}".format(result))
