658453887d72565ff6073b38e10e6afdbd120ad9
WachiraNjoro <dennisnwachira@gmail.com>
2022-09-16 14:31:45 -0700

    Mandatory Task

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
-"""prints all City objects
-from the database hbtn_0e_14_usa"""
+"""
+This module runs a script to fetch everything from the
+cities table with the corresponding state name based
+on the ForeignKey for state id from the the states table
+"""
+from sys import argv
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-
-    import sys
-    from model_state import Base, State
-    from model_city import City
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-    from sqlalchemy.schema import Table
-
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2],
-                                   sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
 
-    session = Session(engine)
-    for state, city in session.query(State, City)\
-                              .filter(City.state_id == State.id)\
-                              .order_by(City.id).all():
-            print("{}: ({}) {}".format(state.name, city.id, city.name))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    get_cities = session.query(State, City)\
+        .filter(City.state_id == State.id).order_by(City.id).all()
+    for state, city in get_cities:
+        # print(city)
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
     session.close()
